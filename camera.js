fd67a13f725f4d95c204aa22c458945dc2fe0f2a
pnhthu <leanhthupham6@gmail.com>
2026-01-01 23:31:53 +0700

    add camera

--- camera.js
+++ camera.js
@@ -4,15 +4,13 @@ const context = canvas.getContext('2d');
 
 // Bật camera
 async function startCamera() {
-  try {
-    const stream = await navigator.mediaDevices.getUserMedia({
-      video: true
-    });
-    video.srcObject = stream;
-  } catch (err) {
-    alert("Không thể truy cập camera!");
-    console.error(err);
-  }
+    const video = document.getElementById('video');
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+      video.srcObject = stream;
+    } catch (e) {
+      alert("Không truy cập được camera");
+    }
 }
 
 // Chụp ảnh từ camera
